c942742dff481382bca9e85c785a9cdebedc7af1
Ichsan Nulmuhlis <ichsan.nulmuhlis@gmail.com>
2022-04-02 10:55:38 +0700

    request

--- controllers/SiteController.php
+++ controllers/SiteController.php
@@ -2,7 +2,9 @@
 
 namespace app\controllers;
 
+use app\core\Application;
 use app\core\Controller;
+use app\core\Request;
 
 class SiteController extends Controller
 {
@@ -19,9 +21,10 @@ class SiteController extends Controller
         return $this->render('contact');
     }
 
-    public function handleContact()
+    public function handleContact(Request $request)
     {
-        var_dump($_POST);
+        $body = $request->getBody();
+        echo var_dump($body);
         return 'Handling Submitted Data';
     }
 }
